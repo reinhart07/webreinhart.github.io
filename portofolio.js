fb809860e2c01cce4a32ee77d245e31e8e06ca22
Reinhart Jens Robert <reinhartrobert23@gmail.com>
2025-04-03 06:23:40 +0800

    portofolio.js

--- portofolio.js
+++ portofolio.js
@@ -1,66 +1,53 @@
 const navLinks = document.getElementById("nav-links");
 const menuBtn = document.getElementById("menu-btn");
 const menuBtnIcon = menuBtn.querySelector("i");
-
 menuBtn.addEventListener("click", (e) => {
   navLinks.classList.toggle("open");
-
   const isOpen = navLinks.classList.contains("open");
   menuBtnIcon.setAttribute(
     "class",
     isOpen ? "ri-close-line" : "ri-menu-3-line"
   );
 });
-
 navLinks.addEventListener("click", (e) => {
   navLinks.classList.remove("open");
   menuBtnIcon.setAttribute("class", "ri-menu-3-line");
 });
-
 const scrollRevealOption = {
   distance: "50px",
   origin: "bottom",
   duration: 1000,
 };
-
 // Reveal elements
-ScrollReveal().reveal(".header__content h1", {
+ScrollReveal().reveal(".headercontent h1", {
   ...scrollRevealOption,
 });
-
-ScrollReveal().reveal(".header__content .section__description", {
+ScrollReveal().reveal(".headercontent .sectiondescription", {
   ...scrollRevealOption,
   delay: 500,
 });
-
-ScrollReveal().reveal(".header__content .header__btn", {
+ScrollReveal().reveal(".headercontent .headerbtn", {
   ...scrollRevealOption,
   delay: 1000,
 });
-
 // Reveal other sections similarly
-
 // about container
-ScrollReveal().reveal(".about__content .section__header", {
+ScrollReveal().reveal(".aboutcontent .sectionheader", {
   ...scrollRevealOption,
 });
-
-ScrollReveal().reveal(".about__content .section__description", {
+ScrollReveal().reveal(".aboutcontent .sectiondescription", {
   ...scrollRevealOption,
   delay: 500,
 });
-
-ScrollReveal().reveal(".about__content .about__btn", {
+ScrollReveal().reveal(".aboutcontent .aboutbtn", {
   ...scrollRevealOption,
   delay: 1000,
 });
-
 // project container
-ScrollReveal().reveal(".project__card", {
+ScrollReveal().reveal(".projectcard", {
   ...scrollRevealOption,
   interval: 500,
 });
-
 // portofolio container
 ScrollReveal().reveal(".portofolio__card", {
   duration: 1000,
